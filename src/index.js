506e19489893843f9f5f2049fca5fd413a4ed158
shilesh-mavchi <mavchi1212@gmail.com>
2024-06-04 12:38:27 +0530

    utils code write wraperclass

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import app from './app.js';
 
 dotenv.config({
     path:'./env'
 })
-connectDB()
 
+connectDB()
+.then(() => {
+    app.on('error',(error)=>{
+        console.error("Error: " + error)
+        throw error;
+    });
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT}`)
+    });
+})
+.catch((err) => {
+    console.error("Mongo db connection failed !!!: " + err)
+    throw err;
+})
 
 
 
